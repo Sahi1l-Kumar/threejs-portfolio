ea1c04fe94c22aef4b3e2abe6305fe9c05b27bd8
Sahi1l-Kumar <2230189@kiit.ac.in>
2025-03-04 04:16:37 +0530

    Fixed salute animation

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
             />
 
             <div>
-              <p className="grid-headtext">Hi, I&apos;m Sahil Kumra</p>
+              <p className="grid-headtext">Hi, I&apos;m Sahil Kumar</p>
               <p className="grid-subtext">
                 With 1 year of experience, I have honed my skills in both
                 frontend and backend dev, creating dynamic and responsive
